c34585a269b33186ec9d2dc3ff1a65f8adf633d9
sunrun-web <67770316+sunrun-web@users.noreply.github.com>
2020-09-23 23:05:46 -0700

    commit on master

--- sendemail.php
+++ sendemail.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "John Doe" );
-define( "RECIPIENT_EMAIL", "youremail@mail.com" );
+define( "RECIPIENT_NAME", "Jesse Flores" );
+define( "RECIPIENT_EMAIL", "mysolarbros@gmail.com" );
 
 // Read the form values
 $success = false;
